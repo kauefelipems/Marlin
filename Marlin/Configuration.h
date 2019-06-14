d0ddceb2157ee47db153ddf6b24fe377efe36849
Kaue <kauefelipems@gmail.com>
2019-06-14 14:51:06 +0100

    Autotuned PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -409,10 +409,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
+  // Peltier
+  #define DEFAULT_Kp 37.16
+  #define DEFAULT_Ki 7.2
+  #define DEFAULT_Kd 47.04
+  	
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
